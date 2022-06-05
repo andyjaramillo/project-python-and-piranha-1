ebdf75692e74272e814f816134a8b47f86ddbca0
Andreas Jaramillo <jaramilloan@lancastercountryday.org>
2022-06-05 16:12:26 -0400

    Everything done except blog

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,3 +11,19 @@ app = Flask(__name__)
 @app.route('/')
 def index(debug=True): #debug=True prevents us from having to restart the server to see changes. REMOVE LATER.
     return render_template('index.html')
+
+@app.route('/blog/')
+def blog(debug=True): 
+    return render_template('blog.html')
+
+@app.route('/about-us/')
+def aboutUs(): 
+    return render_template('aboutUs.html')
+
+@app.route('/contact-us/')
+def contactUs(): 
+    return render_template('contactPage.html')
+
+@app.route('/hobbies/')
+def hobbies(): 
+    return render_template('hobbies.html')
