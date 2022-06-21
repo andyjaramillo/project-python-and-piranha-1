91d95531a74a2b9100e2c0a9b4730a0d9239adfc
Andy Jaramillo <andy.jaramillo022@gmail.com>
2022-06-21 20:29:59 +0000

    redeploy

---
+++ redeploy-site.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+tmux kill-session -a
+cd project-python-and-piranha-1
+cd project-python-and-piranha-1
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+tmux 
+flask run --host=0.0.0.0
+tmux detach
