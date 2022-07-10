68c232f9fe40c56e9a9a5766912dd5a5a9d847a3
andyjaramillo <andyjaramillo@DESKTOP-A4RAQB7.localdomain>
2022-07-09 23:55:58 -0400

    thanks

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,5 +4,5 @@
 cd  project-python-and-piranha-1
 cd  project-python-and-piranha-1
 git fetch && git reset origin/main --hard
-docker compose -f docker compose.prod.yml down
+docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
